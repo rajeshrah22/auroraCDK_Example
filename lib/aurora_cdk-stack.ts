420c2f239d7cbff8f8e658b741699c8a53b930be
rajeshrah22 <rajeshrah22@gmail.com>
2024-03-24 20:22:31 -0400

    yeah

--- lib/aurora_cdk-stack.ts
+++ lib/aurora_cdk-stack.ts
@@ -1,19 +1,42 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_apigatewayv2 } from 'aws-cdk-lib';
+import { aws_lambda } from 'aws-cdk-lib';
+import { aws_rds } from 'aws-cdk-lib';
+import { aws_apigatewayv2_integrations } from 'aws-cdk-lib';
+import { aws_ec2 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class AuroraCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'AuroraCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const vpc = new aws_ec2.Vpc(this, 'auroraVPC');
+
+    // aurora cluster
+    const cluster = new aws_rds.ServerlessCluster(this, 'AuroraTestCluster', {
+      engine: aws_rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
+      parameterGroup: aws_rds.ParameterGroup.fromParameterGroupName(this, 'ParameterGroup',
+          'default.aurora-postgresql10'),
+      defaultDatabaseName: 'TestDB',
+      vpc,
+      scaling: { autoPause: Duration.seconds(0) }
     });
 
-    const topic = new sns.Topic(this, 'AuroraCdkTopic');
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const postFn = new aws_lambda.Function(this, 'MyFunction', {
+      runtime: aws_lambda.Runtime.NODEJS_20_X,
+      code: new aws_lambda.AssetCode('lambda-functions'),
+      handler: 'index.handler',
+      memorySize: 1024,
+      environment: {
+        CLUSTER_ARN: cluster.clusterArn,
+        SECRET_ARN: cluster.secret?.secretArn || '',
+        DB_NAME: 'TestDB',
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1'
+      }
+    })
+
+
+    cluster.grantDataApiAccess(postFn);
   }
 }
